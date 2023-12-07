7dac414ed8c8f8e8b4e28a50a7e7406c4fdf52a7
b4ke <op@mic.quest>
2023-12-06 23:28:58 -0500

    whittling down errors in rendering

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,20 +3,30 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-10 bg-black">
-      <Image
-        width={512}
-        height={512}
-        src="/logo.png"
-        alt="Platforms on Vercel"
-        className="w-48"
-      />
-      <h1 className="text-white">
-        Edit this page on{" "}
-        <InlineSnippet className="ml-2 bg-blue-900 text-blue-100">
-          mic is landed, and outside of the control
-        </InlineSnippet>
-      </h1>
+    <div>
+      <style jsx global>
+      .parent {
+        display: grid;
+          grid-template: auto 1fr auto / auto 1fr auto;
+          }
+      </style>
+        <div className="parent">
+          <header className="section coral">Header</header>
+            <div className="left-side section blue">Left Sidebar</div>
+            <main className="section green">
+              <div className="flex h-screen flex-col items-center justify-center space-y-10 bg-black">
+                  <Image
+                    width={512}
+                    height={512}
+                    src="/logo.png"
+                    alt="Platforms on Vercel"
+                    className="w-48"
+                  />
+                </div>
+            </main>
+            <div class="right-side section yellow">Right Sidebar</div>
+          <footer class="section coral">Footer</footer>
+        </div>
     </div>
   );
 }
